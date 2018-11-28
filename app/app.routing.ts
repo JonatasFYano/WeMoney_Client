38841205f549764a055d1ca0eaaae6517ff15a41
Sousa <jonatas.f.sousa@accenture.com>
2018-11-28 16:50:13 -0200

    first commit

---
+++ app/app.routing.ts
@@ -0,0 +1,24 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+import { AuthGuard } from './_guards/index';
+import { MySessionComponent } from './mySession/mySession.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NewCourseComponent } from './newCourse/newCourse.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'mySession', component: MySessionComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'newCourse', component: NewCourseComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
